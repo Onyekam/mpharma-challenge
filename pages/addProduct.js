954efc1ff16b50193f81bdf0daac6c4d9f8fa044
Michael <onyekami25@gmail.com>
2022-04-20 17:18:45 +0100

    fixed error in addProduct.js regarding useSelector

--- pages/addProduct.js
+++ pages/addProduct.js
@@ -1,20 +1,14 @@
 import Head from 'next/head'
 import NewProductForm from '../components/NewProductForm';
 import { useSelector } from 'react-redux';
-import ProductItem from '../components/ProductItem';
 
 export default function addProduct() {
-  const count = useSelector((state) => state.products.value);
-  const products = useSelector((state) => state.products.products);
-  console.log(products);
   return (
     <div>
         <Head>
             <title>Add New Product</title>
         </Head>
         <h1>Add New Product</h1>
-       
-       
         <NewProductForm />
         
     </div>
